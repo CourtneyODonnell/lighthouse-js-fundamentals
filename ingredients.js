f209382814d61a567a3f470a398226dfc2fbbb21
CourtneyODonnell <courtney.odonnell.cormier@gmail.com>
2022-04-26 22:49:48 -0300

    committing ingredients iterating over arrays exercise.

---
+++ ingredients.js
@@ -0,0 +1,19 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+// Write a while loop that prints out the contents of ingredients:
+
+
+while (ingredients < 1) {
+  console.log(ingredients);
+  i++;
+}
+
+
+// Write a for loop that prints out the contents of ingredients:
+for (i = 0; i < 1; i++) {
+  console.log(ingredients);
+};
+
+// Write any loop (while or for) that prints out the contents of ingredients backwards:
+const reversed = ingredients.reverse();
+console.log('reversed:', reversed);

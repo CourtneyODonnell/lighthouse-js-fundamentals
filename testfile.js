288177411a93dee3d26ada3da29c2a21dd1962b1
CourtneyODonnell <courtney.odonnell.cormier@gmail.com>
2022-04-27 21:31:27 -0300

    committing onjects trash to treasure

---
+++ testfile.js
@@ -0,0 +1,23 @@
+function lastIndexOf(array, num) {
+    return array.lastIndexOf(num);
+}
+
+/* function lastIndexOf(array, num) {
+    if(array.length === 0) {
+    return -1
+    }
+  for (let i = array.length - 1 ; i >= 0; i--) {
+    if(array[i] === num) {
+      return i;
+    }
+    else if (array[i] !== num){
+      return -1;
+    }
+  }
+}
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1), "=?", 3);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 2), "=?", 4);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 3), "=?", -1);
+console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);
+ */
